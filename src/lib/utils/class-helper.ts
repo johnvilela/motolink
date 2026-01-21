6859558ca75feb87b250c1078c3feadb0f913196
John Vilela <johnvvilela@gmail.com>
2026-01-21 15:35:53 -0300

    feat: configurar shadcn

---
+++ src/lib/utils/class-helper.ts
@@ -0,0 +1,6 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function classHelper(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
