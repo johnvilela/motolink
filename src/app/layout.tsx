be132018c13ff6d977ad68985a535fb8c01dc61e
John Vilela <johnvvilela@gmail.com>
2026-02-11 22:44:09 -0300

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
   title: "Sistema Motolink",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
