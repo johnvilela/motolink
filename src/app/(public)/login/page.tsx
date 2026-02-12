58c589531313dfa9d8bf189482d2e8ef0bc7f5a1
John Vilela <johnvvilela@gmail.com>
2026-02-12 02:02:35 -0300

    feat: enhance LoginPage with welcome message

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,5 +1,14 @@
 import { LoginForm } from "@/components/forms/login-form";
+import { Text } from "@/components/ui/text";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <Text className="text-center" variant="muted">
+        Bem-vindo ao Sistema Motolink. Faça login para acessar o painel de
+        controle e gerenciar suas operações de forma simples e eficiente.
+      </Text>
+      <LoginForm />
+    </>
+  );
 }
